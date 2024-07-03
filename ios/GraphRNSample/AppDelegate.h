091529948758f8348511f37ea9637b1bcf9151a0
Paveen <paveen@thecloud.company>
2024-07-03 08:38:18 +0530

    initial commit

---
+++ ios/GraphRNSample/AppDelegate.h
@@ -0,0 +1,8 @@
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
+#import <UserNotifications/UNUserNotificationCenter.h>
+@interface AppDelegate : RCTAppDelegate <RNAppAuthAuthorizationFlowManager , UNUserNotificationCenterDelegate>
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
+
+@end
